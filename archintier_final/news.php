295ed18474b711f9861624eb17c6ede8510922c7
PaulFridrick <paul.fridrick@telecom-st-etienne.fr>
2015-02-01 21:55:11 +0100

    last update

--- archintier_final/news.php
+++ archintier_final/news.php
@@ -8,7 +8,6 @@
 	    <meta charset="utf-8" />
         <link href="css/bootstrap.css" rel="stylesheet">
 	    <link href="css/bootstrap-theme.css" rel="stylesheet">
-	    <link href="css/customize.css" rel="stylesheet">
     </head>
 
 	<body>
